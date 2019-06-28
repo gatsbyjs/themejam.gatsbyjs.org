aefe7671515c2125ca698c49cf77536fe41a6c63
Amberley Romo <amberleyjohanna@gmail.com>
2019-06-28 17:14:56 -0500

    wip

---
+++ site/src/components/Callout.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { jsx } from 'theme-ui';
+import { relative } from 'upath';
+/** @jsx jsx */
+
+const Callout = () => {
+  return (
+    <div
+      sx={{
+        position: `relative`,
+        backgroundColor: `#FCFAFF`,
+        minHeight: `100px`,
+        marginTop: `2rem`,
+        padding: `4rem`,
+        '&::before': {
+          position: `absolute`,
+          zIndex: `-1`,
+          width: `100px`,
+          height: `100px`,
+          transform: `translate(-5.75rem,-5.75rem)`,
+          content: '""',
+          backgroundImage: `linear-gradient(45deg, #B17ACC 16.67%, #ffffff 16.67%, #ffffff 50%, #B17ACC 50%, #B17ACC 66.67%, #ffffff 66.67%, #ffffff 100%)`,
+          backgroundSize: `4.24px 4.24px`,
+        },
+        '&::after': {
+          position: `absolute`,
+          content: '""',
+          bottom: 0,
+          left: 0,
+          transform: `rotate(135deg) translate(21px, -3px)`,
+          borderLeft: `20px solid transparent`,
+          borderRight: `20px solid transparent`,
+          borderTop: `20px solid #B17ACC`,
+        },
+      }}
+    >
+      {`Get started`.toUpperCase()}
+      <p>
+        Lorem ipsum sunt, sicilia est insula. Here's a quick start for those of
+        you who are familiar with Gatsby - I'm not sure about this copy at all.
+      </p>
+
+      <ul>
+        <li>Contest Rules</li>
+        <li>Themes Docs</li>
+        <li>Official Blog Theme on GitHub</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Callout;
--- site/src/gatsby-theme-marketing-sanity/theme.js
+++ site/src/gatsby-theme-marketing-sanity/theme.js
@@ -19,6 +19,7 @@ export const theme = {
   radii: [4, 32],
   sizes: {
     default: '90vw',
+    max: '100%',
   },
   styles: {
     Layout: {
@@ -42,7 +43,7 @@ export const theme = {
     },
     Main: {
       margin: '0 auto',
-      maxWidth: '650px',
+      maxWidth: '100%',
       width: 'default',
     },
     Container: {
